084e0b24d8849e740bb60b456adc19e51fd185c2
ishikasharma871 <ishika.sharma3166@gmail.com>
2026-06-20 19:48:48 +0530

    Refactor revenue percentage calculation for pizza types

--- pizza_sales_analysis.sql
+++ pizza_sales_analysis.sql
@@ -82,14 +82,17 @@ on order_details.pizza_id=pizzas.pizza_id
  order by revenue  DESC limit 3;
 
 -- Calculate the percentage contribution of each pizza type to total revenue.
-select pizza_types.Category ,(sum(order_details.quantity * pizzas.price)/(select
-ROUND(sum( order_details.quantity*pizzas.price),2)
-as total_sales  from order_details join pizzas on pizzas.pizza_id=order_details.pizza_id ))*100 as revenue
-from pizza_types join pizzas 
-on pizza_types.pizza_type_id=pizzas.pizza_type_id
-join order_details 
-on order_details.pizza_id=pizzas.pizza_id
- group by pizza_types.Category  order by revenue  desc;
+SELECT pizza_types.Category,
+       ROUND((SUM(order_details.quantity * pizzas.price) / (
+           SELECT SUM(order_details.quantity * pizzas.price)
+           FROM order_details
+           JOIN pizzas ON pizzas.pizza_id = order_details.pizza_id
+       )) * 100, 2) AS revenue_percentage
+FROM pizza_types
+JOIN pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+JOIN order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.Category
+ORDER BY revenue_percentage DESC;
 
 
 
